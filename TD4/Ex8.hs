f3c264a5bd66d0edaa3117d3408c3a231b2155c0
PhamNhatQuang207 <phamquang207207@gmail.com>
2025-10-22 22:44:21 +0200

    add another solution

--- TD4/Ex8.hs
+++ TD4/Ex8.hs
@@ -1,1 +1,6 @@
 exApprox x n = foldr (\k acc -> 1 + x / fromIntegral k * acc) 1 [1..n]
+
+expApprox x n = res
+  where
+    (_,_,res) = foldl go (1,1,1) [1..n]
+    go (xk,kf,res) k = (xk * x,kf*k,res + (xk*x) / (kf*k))
